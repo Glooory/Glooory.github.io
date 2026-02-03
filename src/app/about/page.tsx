3897aa625c4d252a7c834470e8b767af151b53d2
Glooory <glooorypu@gmail.com>
2026-02-04 07:42:57 +0800

    fix: about page padding

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,11 @@
+import styles from "./styles.module.css";
+
 export default async function Page() {
   const post = await import(`@content/about.mdx`);
   const { default: Post } = post;
 
   return (
-    <article className="markdown-content">
+    <article className={styles.page}>
       <Post />
     </article>
   );

12977331822a0203dbf03616d9f58f8c26adfaa6
Glooory <glooorypu@gmail.com>
2026-03-02 07:41:50 +0800

    feat: add mantine notification

--- content/components/Welcome/Welcome.tsx
+++ content/components/Welcome/Welcome.tsx
@@ -1,9 +1,20 @@
-import { Anchor, Text, Title } from "@mantine/core";
+"use client";
+
+import { Anchor, Button, Text, Title } from "@mantine/core";
+import { notifications } from "@mantine/notifications";
 import classes from "./Welcome.module.css";
 
 export function Welcome() {
+  const showNotification = () => {
+    notifications.show({
+      title: "Default notification",
+      message: "Welcome to my MDX page!",
+    });
+  };
+
   return (
     <>
+      <Button onClick={showNotification}>Show Notification</Button>
       <Title className={classes.title} ta="center" mt={100}>
         Welcome to{" "}
         <Text inherit variant="gradient" component="span" gradient={{ from: "pink", to: "yellow" }}>
